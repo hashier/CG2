795ee9cc62e40e776fbbd0172339dc2a0e8dcd0f
Lutz Reinhardt <lutz.reinhardt@tu-bs.de>
2011-04-19 18:15:56 +0200

    Schonmal ein wenig ObjLoader und MeshObj geschrieben. Aber jetzt gehe ich laufen.

--- ex02/src/MeshObj.cpp
+++ ex02/src/MeshObj.cpp
@@ -18,8 +18,24 @@ MeshObj::~MeshObj() {
 
 void MeshObj::setData(const std::vector<Vertex> &vertexData, const std::vector<unsigned int> &indexData) {
   // TODO: compute the loaded meshes bounds that may be used later on and store them in mMinBounds, mMaxBounds resp. //
+  for (std::vector<Vertex>::const_iterator it = vertexData.begin(); it != vertexData.end(); it++) {
+    Vertex v = *it;
+    for (unsigned int i = 0; i < 3; i++) {
+      if (v.position[i] < mMinBounds[i])
+        mMinBounds[i] = v.position[i];
+      if (v.position[i] > mMaxBounds[i])
+        mMaxBounds[i] = v.position[i];
+    }
+  }
   
   // TODO: COPY the data from vertexData and indexData in own data vectors mVertexData and mIndexData //
+  mVertexData.clear();
+  mVertexData.resize(vertexData.size());
+  std::copy(vertexData.begin(), vertexData.begin(), mVertexData.begin());
+
+  mIndexData.clear();
+  mIndexData.resize(indexData.size());
+  std::copy(indexData.begin(), indexData.begin(), mIndexData.begin());
 }
 
 void MeshObj::render(void) {
--- ex02/src/ObjLoader.cpp
+++ ex02/src/ObjLoader.cpp
@@ -15,13 +15,21 @@ ObjLoader::~ObjLoader() {
 MeshObj* ObjLoader::loadObjFile(std::string fileName, std::string ID) {
   // TODO: check for already existing objects for the given ID string //
   //       if ID is already known, return the corresponding MeshObj instead of creating a new one //
-  
+  if (mMeshMap.find(ID) != mMeshMap.end())
+    return *mMeshMap.find(ID);
   // TODO: parse the given .obj file and create a new MeshObj from the imported data //
-  
+  MeshObj* tmp_mesh = createMeshObj(std::stirng fileName);
   // TODO: insert this MeshObj into the mMeshMap using the given identifier string ID //
+  mMeshMap.insert(std::pair<std::string, MeshObj*>(ID, tmp_mesh));  
+
+  return tmp_mesh;
 }
 
 MeshObj* ObjLoader::getMeshObj(std::string ID) {
   // TODO: return the object using ID //
   // return NULL if an object for the given ID doesn't exist //
+  if (mMeshMap.find(ID) != mMeshMap.end())
+    return *mMeshMap.find(ID);
+  else
+    return NULL;
 }
