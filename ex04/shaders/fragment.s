e3752665507e8bac8807ff2d66e6a77aa43a123a
lutz <lutz@hermann.lan>
2011-05-16 00:57:23 +0200

    fragmentshader repariert
    man sieht jetzt den lichtkegel, nur die richtung der kamera wird noch
    nicht beachtet

--- ex04/shaders/fragment.s
+++ ex04/shaders/fragment.s
@@ -14,13 +14,16 @@ void main () {
 
     // diffuse component //
     float NdotL = max(0.0, dot(N, L));
-    if(NdotL < 0.1f)
-        gl_FragColor = vec4(0.5, 0.5, 0.0, 1.0);
-    else if(NdotL < uni_outerSpotAngle)
-        gl_FragColor = vec4(0.0, 0.5, 0.5, 1.0) * NdotL;
+    // TODO PI suchen
+    float angle = acos(NdotL)*180/3.14;
+    // licht weiß gemacht
+    if(angle < uni_innerSpotAngle)
+        gl_FragColor = vec4(0.5, 0.5, 0.5, 1.0);
+    else if(angle < uni_outerSpotAngle)
+        gl_FragColor = vec4(0.5, 0.5, 0.5, 1.0) * (uni_outerSpotAngle - angle)/(uni_outerSpotAngle - uni_innerSpotAngle);
     else
         gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
 
-    gl_FragColor += vec4(0.2, 0.2, 0.2, 0.0);
+    gl_FragColor += vec4(0.1, 0.1, 0.1, 0.0);
 
 }
