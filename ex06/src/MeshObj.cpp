b922a11d271203ad1223667bfaef8879f62f870e
Matthias Uschok <dev@uschok.de>
2011-05-25 17:49:27 +0200

    Erzeuge und rendere VBO

--- ex06/src/MeshObj.cpp
+++ ex06/src/MeshObj.cpp
@@ -33,25 +33,24 @@ void MeshObj::setData(const std::vector<Vertex> &vertexData, const std::vector<u
   // TODO: copy your data into an vertex and index array (Vertex[], GLuint[]) //
   Vertex vertexArray[vertexData.size()];
   GLuint indexArray[indexData.size()];
-  GLuint vboBuffer;
-  GLuint iboBuffer;
   for(unsigned int i=0; i<vertexData.size(); i++) {
     vertexArray[i] = vertexData[i];
   }
   for(unsigned int i=0; i<indexData.size(); i++) {
     indexArray[i] = indexData[i];
   }
+  mIndexCount = vertexData.size();
   
   // TODO: init and bind a VBO (vertex buffer object) //
-  glGenBuffers(1, &vboBuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, vboBuffer);
+  glGenBuffers(1, &mVBO);
+  glBindBuffer(GL_ARRAY_BUFFER, mVBO);
   
   // TODO: copy data into the VBO //
   glBufferData(GL_ARRAY_BUFFER, vertexData.size()*sizeof(Vertex), vertexArray, GL_STATIC_DRAW);
   
   // TODO: init and bind a IBO (index buffer object) //
-  glGenBuffers(1, &iboBuffer);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboBuffer);
+  glGenBuffers(1, &mIBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIBO);
   
   // TODO: copy data into the IBO //
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexData.size()*sizeof(unsigned int), indexArray, GL_STATIC_DRAW);
@@ -62,12 +61,22 @@ void MeshObj::setData(const std::vector<Vertex> &vertexData, const std::vector<u
 }
 
 void MeshObj::render(void) {
+  const unsigned int vertexInterval = sizeof(Vertex);
+
   if (mVBO != 0) {
     // TODO: init vertex attribute arrays for vertex position, normal vector and texture coordinate //
+    glBindBuffer(GL_ARRAY_BUFFER, mVBO);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_FLOAT, vertexInterval, NULL);
+    glNormalPointer(GL_FLOAT, vertexInterval, NULL);
+    glTexCoordPointer(2, GL_FLOAT, vertexInterval, NULL);
+    glColorPointer(4, GL_FLOAT, vertexInterval, NULL);
     
     // TODO: bind the index buffer object mIBO here to tell OpenGL to use the indices in that array for indexing elements in our vertex buffer //
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIBO);
     
     // TODO: render VBO as triangles //
+    glDrawElements(GL_TRIANGLES, mIndexCount, GL_UNSIGNED_INT, NULL);
     
     // unbind the buffers //
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
