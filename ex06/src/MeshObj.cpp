c6656e6480ca0286786caea5959f032050495458
Matthias Uschok <dev@uschok.de>
2011-05-25 16:55:56 +0200

    VBO erstellt, compiler-warnungen beseitigt

--- ex06/src/MeshObj.cpp
+++ ex06/src/MeshObj.cpp
@@ -23,7 +23,7 @@ void MeshObj::setData(const std::vector<Vertex> &vertexData, const std::vector<u
     mMinBounds[i] = std::numeric_limits<float>::max();
     mMaxBounds[i] = std::numeric_limits<float>::min();
   }
-  for (int i = 0; i < vertexData.size(); ++i) {
+  for (unsigned int i = 0; i < vertexData.size(); ++i) {
     for (int j = 0; j < 3; ++j) {
       if (vertexData[i].position[j] < mMinBounds[j]) mMinBounds[j] = vertexData[i].position[j];
       if (vertexData[i].position[j] > mMaxBounds[j]) mMaxBounds[j] = vertexData[i].position[j];
@@ -31,14 +31,30 @@ void MeshObj::setData(const std::vector<Vertex> &vertexData, const std::vector<u
   }
   
   // TODO: copy your data into an vertex and index array (Vertex[], GLuint[]) //
+  Vertex vertexArray[vertexData.size()];
+  GLuint indexArray[indexData.size()];
+  GLuint vboBuffer;
+  GLuint iboBuffer;
+  for(unsigned int i=0; i<vertexData.size(); i++) {
+    vertexArray[i] = vertexData[i];
+  }
+  for(unsigned int i=0; i<indexData.size(); i++) {
+    indexArray[i] = indexData[i];
+  }
   
   // TODO: init and bind a VBO (vertex buffer object) //
+  glGenBuffers(1, &vboBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vboBuffer);
   
   // TODO: copy data into the VBO //
+  glBufferData(GL_ARRAY_BUFFER, vertexData.size()*sizeof(Vertex), vertexArray, GL_STATIC_DRAW);
   
   // TODO: init and bind a IBO (index buffer object) //
+  glGenBuffers(1, &iboBuffer);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboBuffer);
   
   // TODO: copy data into the IBO //
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexData.size()*sizeof(unsigned int), indexArray, GL_STATIC_DRAW);
   
   // unbind buffers //
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
