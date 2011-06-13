9fc9047b12de58e68e2121b706db51cf0fb61e50
Matthias Uschok <matthias@uschok.de>
2011-06-13 13:03:32 +0200

    Vincents Point3D code kopiert

--- ex08/include/ObjLoader.h
+++ ex08/include/ObjLoader.h
@@ -7,6 +7,64 @@
 
 #include "MeshObj.h"
 
+// copied Vincent's 3D-Vector-struct
+struct Point3D {
+  Point3D(float x = 0, float y = 0, float z = 0) {
+    data[0] = x;
+    data[1] = y;
+    data[2] = z;
+  }
+  Point3D(float const* const d) {
+    data[0] = d[0];
+    data[1] = d[1];
+    data[2] = d[2];
+  }
+  float data[3];
+
+  float operator*(Point3D const &a) {
+      float r = 0.0f;
+      r += this->data[0] * a.data[0];
+      r += this->data[1] * a.data[1];
+      r += this->data[2] * a.data[2];
+      return r;
+  }
+
+  Point3D operator*(float const &a) {
+      Point3D p(this->data);
+      p.data[0] *= a;
+      p.data[1] *= a;
+      p.data[2] *= a;
+      return p;
+  }
+
+  Point3D operator+(Point3D const &a) {
+      Point3D p(this->data);
+      p.data[0] += a.data[0];
+      p.data[1] += a.data[1];
+      p.data[2] += a.data[2];
+      return p;
+  }
+
+  Point3D operator-(Point3D const &a) {
+      Point3D p(this->data);
+      p.data[0] -= a.data[0];
+      p.data[1] -= a.data[1];
+      p.data[2] -= a.data[2];
+      return p;
+  }
+
+  Point3D cross(Point3D const &a) {
+      Point3D p;
+      p.data[0] = this->data[1] * a.data[2] - this->data[2] * a.data[1];
+      p.data[1] = this->data[2] * a.data[0] - this->data[0] * a.data[2];
+      p.data[2] = this->data[0] * a.data[1] - this->data[1] * a.data[0];
+      return p;
+}
+
+};
+
+Point3D operator*(float const &a, Point3D const &b);
+
 struct Point3D {
   Point3D(float x = 0, float y = 0, float z = 0) {
     data[0] = x;
